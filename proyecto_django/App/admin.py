ce00559d7be63b4d1cc4dae85046ab384a7ea65c
Gabriela Gonzalez <gabrielagonzalezpalacio@gmail.com>
2024-12-20 22:22:43 -0300

    cambios en admin con list_display, ordering, search_fields, list_editable y list_filter

--- proyecto_django/App/admin.py
+++ proyecto_django/App/admin.py
@@ -3,6 +3,30 @@ from django.contrib import admin
 # Register your models here.
 from App.models import socios, actividades, sucursales
 
-admin.site.register(socios)
-admin.site.register(actividades)
-admin.site.register(sucursales)
+
+@admin.register(socios)
+class sociosAdmin (admin.ModelAdmin):
+    list_display = ["id", "nombre", "apellido", "dni", "email", "telefono"]
+    ordering = ["apellido"]
+    search_fields = ["nombre", "apellido", "dni"]
+    list_editable = [ "nombre", "apellido", "dni", "email", "telefono"]
+    list_filter = ( "apellido",)
+
+
+@admin.register(actividades)
+class actividadesAdmin (admin.ModelAdmin):
+    list_display = [ "id", "nombre", "horario", "profesor", "descripcion"]
+    ordering = ["id"]
+    search_fields = ["nombre", "profesor"]
+    list_editable = [ "nombre", "horario", "profesor", "descripcion"]
+    list_filter = ("nombre", "profesor")
+
+
+
+@admin.register(sucursales)
+class sucursalesAdmin (admin.ModelAdmin):
+    list_display = ["id", "nombre", "direccion"]
+    ordering = ["id"]
+    search_fields = ["nombre", "direccion"]
+    list_editable = [ "nombre", "direccion"]
+    list_filter = ("nombre",)
--- proyecto_django/App/models.py
+++ proyecto_django/App/models.py
@@ -8,14 +8,24 @@ class socios(models.Model):
     email = models.EmailField()
     telefono = models.CharField(max_length=20)
 
+    def __str__(self):
+        return {self.id}|{self.nombre} 
+
 class actividades(models.Model):
     nombre = models.CharField(max_length=30)
     horario = models.TimeField()
     profesor = models.CharField(max_length=50)
     descripcion = models.TextField()
 
+    def __str__(self):
+        return {self.id}
+
+
 
 class sucursales(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return {self.id}
 
